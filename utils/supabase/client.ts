46a5718eb7031936f2bfda0c7aa74f965173f8f7
GAKUNEMOTO <nemotogaku1@gmail.com>
2025-03-03 22:01:07 -0600

    引数にauthを追加して、随時userを確認

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -3,7 +3,13 @@ import { createBrowserClient } from '@supabase/ssr'
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true, // セッションをローカルストレージ等に保持
+      autoRefreshToken: true, // アクセストークン自動更新
+      detectSessionInUrl: true,
+    },
+  },
   )
 }
-
